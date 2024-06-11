cb0e21d08dc563abcf3d8a9155210e57abeb2457
suryadevisetty <suryadevisetty9999@gmail.com>
2024-06-11 11:42:15 +0530

    Update s3.tf

--- modules/s3.tf
+++ modules/s3.tf
@@ -9,29 +9,3 @@ module "s3" {
 
 
 
-
-
-module "iam" {
-  source     = "./iam"
-  role       = var.role_name
-  depends_on = [module.s3]
-
-}
-
-
-module "vpc" {
-  source             = "./vpc"
-  cidr               = var.cidr
-  vpc_name           = var.vpc_name
-  public_subnet_cidr = var.public_subnet_cidr
-  public_subnet_name = var.public_subnet_name
-}
-
-module "ec2" {
-  source        = "./ec2"
-  subnetid      = module.vpc.public_subnet_id
-  ami           = "ami-07ef4004db979fcd4"
-  instance_type = "t2.micro"
-
-
-}
